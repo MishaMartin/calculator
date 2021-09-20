087d07a08660c0b621b896f9692b0a75314d54d2
Misha Martin <mishanicolemartin@gmail.com>
2021-09-20 16:24:24 -0600

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,51 @@
+const { SSL_OP_NO_QUERY_MTU } = require('constants');
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+    tokens = input.split(' ');
+
+    mathSymbol = tokens[0];
+    num1 = Number(tokens[1]);
+    num2 = Number(tokens[2]);
+
+
+    if (mathSymbol === "+"){
+      console.log(num1 + num2);
+}
+
+reader.close();
+
+if (mathSymbol === "-"){
+  console.log(num1 - num2);
+}
+
+reader.close();
+if (mathSymbol === "*"){
+  console.log(num1 * num2);
+}
+
+reader.close();
+if (mathSymbol === "/"){
+  console.log(num1 / num2);
+}
+
+reader.close();
+if (mathSymbol === "sqrt"){
+  console.log(Math.sqrt(num1));
+}
+
+reader.close();
+
+    // This line closes the connection to the command line interface.
+    // reader.close()
+
+});
+
+
