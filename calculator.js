6345f5fcbaaa74abcf505f94d8c93eb24a3a379a
Misha Martin <mishanicolemartin@gmail.com>
2021-09-20 20:45:23 -0600

    Addition implementation working

--- calculator.js
+++ calculator.js
@@ -14,31 +14,59 @@ reader.question("What would you like to calculate?", function(input){
     mathSymbol = tokens[0];
     num1 = Number(tokens[1]);
     num2 = Number(tokens[2]);
+    num3 = Number(tokens[3]);
 
 
     if (mathSymbol === "+"){
-      console.log(num1 + num2);
+      console.log(num1 + num2 + num3);
 }
 
 reader.close();
 
 if (mathSymbol === "-"){
-  console.log(num1 - num2);
+  console.log(num1 - num2 - num3);
 }
 
 reader.close();
+
 if (mathSymbol === "*"){
-  console.log(num1 * num2);
+  console.log(num1 * num2 * num3);
 }
 
 reader.close();
+
 if (mathSymbol === "/"){
-  console.log(num1 / num2);
+  console.log(num1 / num2 / num3);
 }
 
 reader.close();
+
 if (mathSymbol === "sqrt"){
   console.log(Math.sqrt(num1));
+}
+
+reader.close();
+
+if (mathSymbol === "sq"){
+  console.log(Math.pow(num1,2));
+}
+
+reader.close();
+
+if (mathSymbol === "cube"){
+  console.log(Math.pow(num1,3));
+}
+
+reader.close();
+
+if (mathSymbol === "exp"){
+  console.log(Math.pow(num1,num2));
+}
+
+reader.close();
+
+if (mathSymbol === "rem"){
+  console.log(num1 % num2);
 }
 
 reader.close();
